0246ce7c1553b9c6d4a3dd56b3e69bb586d8fb10
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-28 18:09:46 +0000

    c

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for a in range(0, 9):
-   for c in range(a + 1, 10):
-        if a != 8:
-            print("{:d}{:d}".format(a, c), end=', ')
+for i in range(0, 9):
+    for c in range(i + 1, 10):
+        if i != 8:
+            print("{:d}{:d}".format(i, c), end=', ')
         else:
-            print("{:d}{:d}".format(a, c)
+            print("{:d}{:d}".format(i, c)
