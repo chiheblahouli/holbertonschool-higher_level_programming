a40d4bb5dcb4c32c92fa554d8f03ce7fbf84e1ba
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-28 17:59:42 +0000

    c

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/python3
-for i in range (100):
-    if i == 99:
-       print('{:02d}'.format(i))
+for j in range(100):
+    if j != 99:
+      print('{:02d}'.format(j), end=", ")
     else:
-       print('{:02d}'.format(i), end=", ")
+      print('{:02d}'.format(j))
