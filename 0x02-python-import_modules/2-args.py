f011588d7d18aa0be886eb57120a941f3821c68f
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-30 16:28:45 +0000

    arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    list = sys.argv
+    cross = len(list) - 1
+    if cross == 0:
+        print("{:d} arguments.".format(cross))
+    elif cross == 1:
+        print("{:d} argument:".format(cross))
+    else:
+        print("{:d} arguments:".format(cross))
+    if numArg > 0:
+        for i in range(len(list)):
+            if i > 0:
+                print("{:d}: {}".format(i, list[i]))
