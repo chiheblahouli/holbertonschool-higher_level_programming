9ead7acbb73236d9b6c461e2a1e56824332319fb
chiheblahouli <chiheb.lahouli@gmail.com>
2020-05-11 20:44:04 +0000

    c

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_list = set()
+    new_list = dict()
     for key, elay in a_dictionary.items():
         new_list.update({key: (elay * 2)})
     return new_list
