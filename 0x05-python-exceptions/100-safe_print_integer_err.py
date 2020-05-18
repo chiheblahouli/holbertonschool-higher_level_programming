d7eac132d8735ff6b4465ca604f5fa1d6531d476
chiheblahouli <chiheb.lahouli@gmail.com>
2020-05-18 20:20:52 +0000

    c

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as error:
+        return True
+        print("Exception: {}".format(error), file=sys.stderr)
+
+    else:
+        return False
