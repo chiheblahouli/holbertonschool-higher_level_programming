43efa0ed129d6bc0babaebafd51c07bcab41f4aa
chiheblahouli <chiheb.lahouli@gmail.com>
2020-05-19 20:40:58 +0000

    c

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 
-class Square:
-    """
-        A Square class that will initialize and store values in it's
-        private attribute __self
-        Attribute:
-             __size will contain the value of size
-    """
 
+
+class Square:
+    """a class Square that defines as a square"""
     def __init__(self, size=0):
-        """
-            __init__ will initialize the private attribute __size with size
-            size must be an integer type and > than 0
-        """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
+        """instantiation with size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
