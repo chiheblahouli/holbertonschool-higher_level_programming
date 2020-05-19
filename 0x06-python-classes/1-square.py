6b08386efcbfaca52fb827b31209a6e7d01d2609
chiheblahouli <chiheb.lahouli@gmail.com>
2020-05-19 20:30:56 +0000

    c

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
-class Square:
-    """define a square"""
+class Square():
+    """ A square class """
     def __init__(self, size):
-        """Initializes the square """
+        """ Initializing class """
         self.__size = size
