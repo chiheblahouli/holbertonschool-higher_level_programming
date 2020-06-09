ce01d7e6dc4bf5ec19535287ba0f3c148d56ce4f
chiheblahouli <chiheb.lahouli@gmail.com>
2020-06-09 11:13:06 +0000

    c

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that return true if object is exactly
+"""
+
+
+def is_same_class(obj, a_class):
+    """return true if obj is the exact class a_class, otherwise false"""
+    return (type(obj) == a_class)
