0ecc70c2c0f4cd09a3ba8d190acc7624bbc2a105
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-02 14:04:16 +0000

    c

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+create an object from JSON file
+"""
+import json
+
+
+
+def load_from_json_file(filename):
+    """
+    write a file
+    """
+
+    with open(filename, 'r', encoding='utf8') as Fichier:
+        return json.load(Fichier)
