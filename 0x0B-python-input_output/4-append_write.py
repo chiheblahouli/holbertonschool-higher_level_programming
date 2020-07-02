96f534e017275c050116aa0c975495dba4952b50
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-02 13:01:07 +0000

    c

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -8,5 +8,5 @@ prints text
 def append_write(filename="", text=""):
     """ write text """
     with open(filename, 'a', encoding='utf8') as Fichier:
-       N = file.write(text)
+       N = Fichier.write(text)
     return(N)
