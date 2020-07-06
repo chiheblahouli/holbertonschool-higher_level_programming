e01a8c229c52f132c70ee11db87a70095f9dd729
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-06 12:21:26 +0000

    c

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' a function that returns a list of integers'''
+
+
+def pascal_triangle(n):
+    ''' a list of lists of integer pascal's triangle '''
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+
+    return triangle
