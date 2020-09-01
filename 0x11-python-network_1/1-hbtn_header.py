ed1bece367d563c771e35b4234f8975fa5bcfbbf
chiheblahouli <chiheb.lahouli@gmail.com>
2020-09-01 11:49:50 +0100

    c

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+displays the value of the X-Request-Id
+"""
+if __name__ == "__main__":
+import urllib.request
+    from sys import argv
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
