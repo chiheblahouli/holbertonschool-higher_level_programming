18040440c62294cdce4935594666e694544bdb8d
chiheblahouli <chiheb.lahouli@gmail.com>
2020-12-17 12:29:12 +0100

    c

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-a script that list databases
+A script that list all states from the database
 """
 
 
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", passwd=argv[2], user=argv[1],
                          db=argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
