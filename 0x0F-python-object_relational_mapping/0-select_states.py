42ae6c65fd26251455b3039715a607094d07409a
chiheblahouli <chiheb.lahouli@gmail.com>
2020-12-17 12:35:14 +0100

    c

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,15 @@
 a script that list databases
 """
 
-
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", passwd=argv[2], user=argv[1],
-                         db=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
