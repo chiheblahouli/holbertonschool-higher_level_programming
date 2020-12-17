2141f12e8ea30a6838a9b2a5dec88a8f95964dc5
chiheblahouli <chiheb.lahouli@gmail.com>
2020-12-17 13:28:43 +0100

    c

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py10-model_state_my_get.py
@@ -4,22 +4,21 @@ script that lists all state .
 """
 
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                            argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    first_state = session.query(State).first()
-    if first_state is not None:
-        print("{}: {}".format(inst.id, inst.name))
+
+    session = Session(engine)
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+        print(str(state.id))
     else:
-        print("Nothing")
-  session.close()
-
-
+        print("Not found")
+    session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,22 +4,20 @@ script that lists all state .
 """
 
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                            argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    first_state = session.query(State).first()
-    if first_state is not None:
-        print("{}: {}".format(inst.id, inst.name))
-    else:
-        print("Nothing")
-  session.close()
 
+    session = Session(engine)
 
+    for state in session.query(State).order_by(State.id).filter(
+            State.name.contains('a')).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
