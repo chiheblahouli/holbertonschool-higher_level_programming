81e70210247796c6f433f123fe60c21320350976
chiheblahouli <chiheb.lahouli@gmail.com>
2021-01-11 13:46:33 +0100

    c

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,9 +7,9 @@ import urllib.request as request
 import sys
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+    req = request.Request(argv[1])
     try:
-            with urllib.request.urlopen(req) as response:
-                    print(response.read().decode("utf-8"))
-    except urllib.error.URLError as e:
-            print("Error code: {}".format(e.code))
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
