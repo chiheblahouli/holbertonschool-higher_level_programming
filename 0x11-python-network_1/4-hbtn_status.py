2970774bfd9197ea2bb7891f5eb6271e53a56db3
chiheblahouli <chiheb.lahouli@gmail.com>
2021-01-11 13:56:21 +0100

    c

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,10 @@ script that fetches an url using requests package
 """
 import requests
 
-answer = requests.get('https://intranet.hbtn.io/status')
-print("Body answer:\n\
-\t- type: {}\n\
-\t- content: {}".format(type(answer.text), answer.text))
+import requests
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
