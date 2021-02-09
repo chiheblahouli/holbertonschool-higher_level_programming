42e84129c0061e249909d173913560e1a5b934a3
chiheblahouli <chiheb.lahouli@gmail.com>
2021-02-09 13:06:38 +0100

    c

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,11 +3,9 @@ const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
   }
+  fs.writeFile(file, body, function (err) { if (err) console.log(err); });
 });
