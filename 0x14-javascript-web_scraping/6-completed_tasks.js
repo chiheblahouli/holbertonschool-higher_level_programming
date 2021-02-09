86bc7df7de2944807f8f15ce19c3145ed40c103e
chiheblahouli <chiheb.lahouli@gmail.com>
2021-02-09 13:11:53 +0100

    c

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,3 +2,15 @@
 
 const request = require('request');
 const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) { console.log(err); } else {
+    const done = {};
+    for (const i of JSON.parse(body)) {    for (const i of JSON.parse(body)) {
+        if (done[i.userId] === undefined) {
+          done[i.userId] = 1;
+        } else { done[i.userId] = done[i.userId] + 1; }
+      }
+    }
+    console.log(done);
+  }
+});
