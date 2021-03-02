a6acefba38481c57ce5b8f76ab5871a0bdb30c53
chiheblahouli <chiheb.lahouli@gmail.com>
2021-03-02 14:02:07 +0100

    c

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-api to retrieve my github accoun
+use the Github API to retrieve my github account id
 """
-if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
     print(r.json().get('id'))
