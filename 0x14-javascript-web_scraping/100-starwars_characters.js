3b1ddd7e4b3590c00043b7d288290c2174fb0e96
chiheblahouli <chiheb.lahouli@gmail.com>
2021-03-10 12:47:17 +0100

    c

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,13 +2,26 @@
 const request = require('request');
 const film = process.argv[2];
 let url = 'http://swapi.co/api/people/';
-request(url, function (error, response, body) {
-  if (error) { return console.log(error); } else {
-    for (const star of JSON.parse(body).characters) {
-      request(star, function (err, res, bod) {
-        if (err) { return console.log(err); }
-        console.log(JSON.parse(bod).name);
-      });
+function filmcharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let people = jsonobj.results;
+      for (let i in people) {
+        for (let j in people[i].films) {
+          if (people[i].films[j].includes(film)) {
+            console.log(people[i].name);
+          }
+        }
+      }
+      if (jsonobj.next !== null) {
+        filmcharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('An error occured. Status code: ' + response.statusCode);
     }
-  }
-});
+  });
+}
+filmcharacters(film, url);
